8f32b112f3a53c6525419772257d942ce2fe88a0
M. Anthony Aiello <m.anthony.aiello@gmail.com>
2025-06-16 08:13:19 -0400

    WIP to improve the proofs for autonomous mode
    The proofs for autonomous mode were weaker than intended. Notably, there
    should have been assertions of the safety property within the
    `Go_Forward` loop, but these were not added. Adding them revealed a
    number of issues, which I am in the process of resolving.

--- src/rover-autonomous.adb
+++ src/rover-autonomous.adb
@@ -28,18 +28,28 @@ is
       This.User_Exit := (for some B in Buttons => State (B));
    end Check_User_Input;
 
+   Distance_Threshold : constant := 40;
+
    ----------------
    -- Go_Forward --
    ----------------
 
    procedure Go_Forward (This : in out Auto_State) with
      Pre  => Initialized and then
-             Rover.Cannot_Crash,
-     Post => Initialized
+             Rover_HAL.Get_Sonar_Distance >= Distance_Threshold,
+     Post => Initialized and then
+             Rover.Cannot_Crash
    is
-      Distance : Unsigned_32;
-   begin
+      Distance : Unsigned_32 := Distance_Threshold;
+      --  The initial value of Distance (which becomes Last_Distance) is at
+      --  worst Distance_Threhold, by our precondition. I.e., if we
+      --  instantaneously read the sonar at the start of the subprogram, it
+      --  would return a value >= Distance_Threshold.
 
+      Last_Distance : Unsigned_32 with Ghost;
+
+      Sonar_Sampling_Delay : constant := 40;
+   begin
       --  Go forward...
       Set_Turn (Straight);
       Set_Power (Left, 100);
@@ -47,18 +57,26 @@ is
 
       --  Rotate the mast and check for obstacle
       loop
-
          Check_User_Input (This);
-
          exit when This.User_Exit;
 
          Mast_Control.Next_Mast_Angle (This.Mast, -60, 70, 16);
 
+         Last_Distance := Distance;
          Distance := Sonar_Distance;
 
-         exit when Distance < 40;
+         Rover_Displacement_Model
+           (Distance, Last_Distance, Sonar_Sampling_Delay);
+         --  We invoke the Rover displacement model so that SPARK knows the
+         --  limits on how far the rover can have traveled since the last
+         --  distance measurement.
 
-         Delay_Milliseconds (40);
+         exit when Distance < Distance_Threshold;
+
+         pragma Loop_Invariant (Distance >= Distance_Threshold);
+         pragma Loop_Invariant (Rover.Cannot_Crash);
+
+         Delay_Milliseconds (Sonar_Sampling_Delay);
       end loop;
    end Go_Forward;
 
@@ -90,12 +108,38 @@ is
      with
       Pre  => Initialized,
       Post => Initialized and then
-              Rover.Cannot_Crash
+              (This.User_Exit or else
+               Rover_HAL.Get_Sonar_Distance >= Distance_Threshold)
    is
       Left_Dist : Unsigned_32 := 0;
       Right_Dist : Unsigned_32 := 0;
 
       Timeout, Now : Time;
+
+      function Distance_Straight_Ahead return Unsigned_32 with
+         Side_Effects,
+         Pre  => Initialized,
+         Post => Initialized and then
+                 --  We have to make this promise because otherwise SPARK
+                 --  cannot know that the result actually came from the
+                 --  sonar.
+                 Rover_HAL.Get_Sonar_Distance = Distance_Straight_Ahead'Result
+
+      is
+      begin
+         Rover_HAL.Set_Mast_Angle (0);
+         --  Set the mast to the straight position - this can take a bit of
+         --  time, so:
+         Delay_Milliseconds (50);
+
+         return Distance : Unsigned_32 do
+            Distance := Sonar_Distance;
+         end return;
+         --  Extended return used because SPARK won't allow a simple return
+         --  statement here, because the function has side effects.
+      end Distance_Straight_Ahead;
+
+      Distance : Unsigned_32;
 
    begin
       Now := Clock;
@@ -105,46 +149,52 @@ is
       Set_Power (Left, 0);
       Set_Power (Right, 0);
 
-      --  Turn the mast back and forth and log the dected distance for the left
-      --  and right side.
-      loop
-         Check_User_Input (This);
-         Now := Clock;
+      --  Measure the distance straight ahead
+      Distance := Distance_Straight_Ahead;
 
-         exit when This.User_Exit or else Now > Timeout;
+      while Distance < Distance_Threshold and not This.User_Exit loop
+         --  Turn the mast back and forth and log the dected distance for the
+         --  left and right side.
+         loop
+            Check_User_Input (This);
+            Now := Clock;
 
-         Mast_Control.Next_Mast_Angle (This.Mast, -60, 70, 4);
+            exit when This.User_Exit or else Now > Timeout;
 
-         if Mast_Control.Last_Angle (This.Mast) <= -40 then
-            Left_Dist := Sonar_Distance;
+            Mast_Control.Next_Mast_Angle (This.Mast, -60, 70, 4);
+
+            if Mast_Control.Last_Angle (This.Mast) <= -40 then
+               Left_Dist := Sonar_Distance;
+            end if;
+            if Mast_Control.Last_Angle (This.Mast) >= 40 then
+               Right_Dist := Sonar_Distance;
+            end if;
+
+            Delay_Milliseconds (30);
+         end loop;
+
+         if Now > Timeout then
+            if Left_Dist < 50 and then Right_Dist < 50 then
+               --  Obstacles left and right, turn around to find a new direction
+               Turn_Around;
+
+            elsif Left_Dist > Right_Dist then
+               --  Turn left a little
+               Set_Turn (Around);
+               Set_Power (Left, -100);
+               Set_Power (Right, 100);
+               Delay_Milliseconds (800);
+            else
+               --  Turn right a little
+               Set_Turn (Around);
+               Set_Power (Left, 100);
+               Set_Power (Right, -100);
+               Delay_Milliseconds (800);
+            end if;
+
+            Distance := Distance_Straight_Ahead;
          end if;
-         if Mast_Control.Last_Angle (This.Mast) >= 40 then
-            Right_Dist := Sonar_Distance;
-         end if;
-
-         Delay_Milliseconds (30);
       end loop;
-
-      if Now > Timeout then
-         if Left_Dist < 50 and then Right_Dist < 50 then
-            --  Obstacles left and right, turn around to find a new direction
-            Turn_Around;
-
-         elsif Left_Dist > Right_Dist then
-            --  Turn left a little
-            Set_Turn (Around);
-            Set_Power (Left, -100);
-            Set_Power (Right, 100);
-            Delay_Milliseconds (800);
-         else
-            --  Turn right a little
-            Set_Turn (Around);
-            Set_Power (Left, 100);
-            Set_Power (Right, -100);
-            Delay_Milliseconds (800);
-         end if;
-      end if;
-
    end Find_New_Direction;
 
    ---------
@@ -164,8 +214,12 @@ is
 
       while not State.User_Exit loop
 
-         Go_Forward (State);
          Find_New_Direction (State);
+
+         --  The user could have asked to exit the autonomous mode by now
+         exit when State.User_Exit;
+
+         Go_Forward (State);
 
          pragma Loop_Invariant (Rover.Cannot_Crash);
       end loop;
--- src/rover.ads
+++ src/rover.ads
@@ -1,8 +1,9 @@
 with Interfaces;
 with Rover_HAL;
 package Rover with SPARK_Mode is
+   use Interfaces;
 
-   Safety_Distance : constant Interfaces.Unsigned_32 := 20;
+   Safety_Distance : constant Unsigned_32 := 20;
 
    function Cannot_Crash return Boolean
      with
@@ -15,8 +16,31 @@ package Rover with SPARK_Mode is
    --  Safety Requirement: The Mars Rover shall not proceed straight forward
    --  when the distance to an obstacle is less than the Safety Distance.
 
+
+   Max_Speed : constant Float := 0.5;
+   --  The maximum speed of the rover in units of distance per millisecond.
+
+   function Max_Distance
+     (Last_Distance : Unsigned_32;
+      Ellapsed_Time : Unsigned_32)
+   return Unsigned_32
+     with
+       Ghost;
+
+   procedure Rover_Displacement_Model
+     (Distance : in Unsigned_32;
+      Last_Distance : in Unsigned_32;
+      Ellapsed_Time : in Unsigned_32)
+   with
+     Global => null,
+     Post => Distance >= Max_Distance (Last_Distance, Ellapsed_Time),
+     Ghost,
+     Import,
+     Always_Terminates;
+
+
 private
-   use type Interfaces.Unsigned_32;
+   use type Unsigned_32;
    use type Rover_HAL.Turn_Kind;
    use type Rover_HAL.Motor_Power;
 
@@ -26,5 +50,18 @@ private
       then
          Rover_HAL.Get_Power (Rover_HAL.Left)  <= 0 and then
          Rover_HAL.Get_Power (Rover_HAL.Right) <= 0);
+
+   function Max_Distance
+     (Last_Distance : Unsigned_32;
+      Ellapsed_Time : Unsigned_32)
+   return Unsigned_32 is
+     (declare
+        Displacement : constant Unsigned_32 :=
+          Unsigned_32 (Max_Speed * Float (Ellapsed_Time));
+     begin
+        (if Last_Distance < Displacement then
+           0
+        else
+           Last_Distance - Displacement));
 
 end Rover;
